2e52a930a570c3abc5a2d8fca03834f718d6fa8d
Aditya Naik <ADITYANAIK5736@GMAIL.COM>
2025-06-22 15:23:31 +0530

    Add files via upload

---
+++ C-CPP-Java-Logic-Building/18_Assignment/Assignment18_1.c
@@ -0,0 +1,60 @@
+/* Programm to accept N number from user and return difference between summation
+of even elemt and summation of odd elements.
+    input = 6
+            elements - 85 66 3 80 93 88
+    output- 53 (234 -181)
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int Difference(int Arr[], int iLength)
+{
+    int sumEven =0, sumOdd = 0, iCnt = 0;
+
+    for( iCnt = 0; iCnt < iLength;iCnt++)
+    {
+        if((Arr[iCnt] % 2) == 0)
+        {
+            sumEven = sumEven + Arr[iCnt];
+        }
+        else
+        {
+            sumOdd = sumOdd + Arr[iCnt];
+        }
+    }
+    return(sumEven - sumOdd);
+
+}
+
+int main()
+{
+    int iSize = 0, iRet = 0, iCnt = 0;
+    int *p = NULL;
+
+    printf("Enter the number of elements : ");
+    scanf("%d", &iSize);
+
+    p = (int *) malloc (iSize * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("unable to allocate memory");
+        return -1;
+    }
+
+    printf("Enter %d elements \n", iSize);
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        printf("Enter element %d : ",iCnt + 1);
+        scanf("%d",&p[iCnt]);
+    }
+
+    
+    iRet = Difference(p,iSize);
+    printf("Result is %d",iRet);
+
+    free(p);
+
+    return 0;
+}
---
+++ C-CPP-Java-Logic-Building/18_Assignment/Assignment18_2.c
@@ -0,0 +1,56 @@
+/* Programm to accept N number from user and display all elements which are divisible by 5
+    input = 6
+            elements - 85 66 3 80 93 88
+    output- 85 80
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int Divisible(int Arr[], int iLength)
+{
+    int Divide = 0, iCnt = 0, iCount = 0;
+
+    for( iCnt = 0; iCnt < iLength;iCnt++)
+    {
+        if((Arr[iCnt] % 5) == 0)
+       {
+         
+         printf("%d\t",Arr[iCnt]);
+       }
+    }
+    return 0;
+
+}
+
+int main()
+{
+    int iSize = 0, iRet = 0, iCnt = 0;
+    int *p = NULL;
+
+    printf("Enter the number of elements : ");
+    scanf("%d", &iSize);
+
+    p = (int *) malloc (iSize * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("unable to allocate memory");
+        return -1;
+    }
+
+    printf("Enter %d elements \n", iSize);
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        printf("Enter element %d : ",iCnt + 1);
+        scanf("%d",&p[iCnt]);
+    }
+
+    
+    Divisible(p,iSize);
+    
+
+    free(p);
+
+    return 0;
+}
