ce6967b15a31fcbbfda33a9dfc1aa9ea684ce4f0
Aditya Naik <ADITYANAIK5736@GMAIL.COM>
2025-06-22 15:18:32 +0530

    Add files via upload

---
+++ C-CPP-Java-Logic-Building/12_Assignment/Assignment12_5.c
@@ -0,0 +1,47 @@
+/* Programm to accept a number from user di andsplay pattern
+    input = iRow-3   iCol-4
+    output- *   #   *   #
+            *   #   *   #
+            *   #   *   #
+            *   #   *   #      
+*/
+
+#include<stdio.h>
+
+int Pattern(int iRow, int iCol)
+{
+    int iCnt1 = 0, iCnt2 = 0;
+
+    for(iCnt1 = 0; iCnt1 < iRow; iCnt1++)
+    {
+        for(iCnt2 = 1; iCnt2 <= iCol; iCnt2++)
+        {
+            if((iCnt2 % 2) == 0)
+            {
+                printf("#\t",iCnt2);
+            }
+            else
+            {
+                printf("*\t",iCnt2);
+            }
+               
+        }
+        printf("\n");
+    }
+
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter the number rows : ");
+    scanf("%d", &iValue1);
+
+    printf("Enter the number column : ");
+    scanf("%d", &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
